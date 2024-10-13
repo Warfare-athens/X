223654e35560226eaa83e83e6b47251df7c186be
Athens Dubey <athensdubey@gmail.com>
2024-10-13 16:40:05 +0530

    updated code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+// Load environment variables from .env file
+dotenv.config();
+
+const backendUrl = process.env.VITE_BACKEND_URL || "http://localhost:5000";
+
 export default defineConfig({
-	resolve: {
-		alias: {
-		  "@": "/src",
-		  "@components": "/src/components",
-		  "@utils": "/src/utils",
-		},
-	  },
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
-				changeOrigin: true,
-			},
-		},
-	},
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@utils": "/src/utils",
+    },
+  },
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: backendUrl,
+        changeOrigin: true,
+      },
+    },
+  },
 });
